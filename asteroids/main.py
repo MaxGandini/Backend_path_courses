4c86e900eb4ccc7bcc5de6adf0811f033a1265a3
MaxGandini <maximiliano.gandini.27@gmail.com>
2025-02-17 18:56:48 +0000

    up_asteroids

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,21 +1,45 @@
 import pygame  # noqa: F401
+from circleshape import *  # noqa: F403
+from player import Player # noqa: F403
 from constants import *  # noqa: F403
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt=0
+    updatable= pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable,drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)  # noqa: F405
     print("Screen height:", SCREEN_HEIGHT)  # noqa: F405
+
+    screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])  # noqa: F405
+    x = SCREEN_WIDTH/2  # noqa: F405
+    y = SCREEN_HEIGHT/2  # noqa: F405
+
+    player_0 = Player(x,y)
+    asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])  # noqa: F405
         screen.fill((0,0,0))
-        pygame.display.flip()
+        delta = clock.tick(60)/1000
+        updatable.update(delta)
+        for draw in drawable:
+            draw.draw(screen)
 
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
---
+++ asteroids/player.py
@@ -0,0 +1,42 @@
+from constants import *  # noqa: F403
+from circleshape import *
+import pygame
+
+class Player(CircleShape):  # noqa: F405
+    def __init__(self, x, y):
+        super().__init__(x,y,PLAYER_RADIUS)  # noqa: F405
+        self.x = x
+        self.y = y
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)  # noqa: F405
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5  # noqa: F405
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen,"white",self.triangle(),2)
+        pass
+
+    def rotate(self,dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        pass
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self,dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
