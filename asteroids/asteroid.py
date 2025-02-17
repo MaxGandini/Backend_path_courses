2b46266b671daa91196309ba811a63e58c6c9715
MaxGandini <maximiliano.gandini.27@gmail.com>
2025-02-17 19:19:35 +0000

    up_ast

--- asteroids/asteroid.py
+++ asteroids/asteroid.py
@@ -5,8 +5,9 @@ class Asteroid(CircleShape):
     def __init__(self,x,y,radius):
         super().__init__(x,y,radius)
 
-    def draw(self):
-        pygame.draw.circle(self.x,self.y,2)
+    def draw(self,screen):
+        x,y = self.position
+        pygame.draw.circle(screen,"white",(x,y),self.radius,2)
 
     def update(self,dt):
-        self.postion += self.velocity * dt
+        self.position += self.velocity * dt
